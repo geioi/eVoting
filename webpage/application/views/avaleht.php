3476fba1daffe08cfde96b387323c9c2f46aa1f1
Maria <mariabelinskaa@gmail.com>
2017-03-16 16:41:44 +0200

    parandus

--- webpage/application/views/avaleht.php
+++ webpage/application/views/avaleht.php
@@ -1,12 +1,10 @@
  
 <div class="container">
-  <p>Avalehe info</p>
+  <p>Jaoskonnad</p>
   <?php echo $map['js']; ?>
   <?php echo $map['html']; ?>
+  <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDfopMg31GEOWmiqbWs-lqa-Bu_aCf4XFE&callback=initMap" type="text/javascript"></script>
 </div>
-<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDfopMg31GEOWmiqbWs-lqa-Bu_aCf4XFE
-&callback=initMap"
-  type="text/javascript"></script>
 </body>
 </html>
 
