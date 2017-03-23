2fedd46f688de51769c2c30e09e465811cfae395
Maria <mariabelinskaa@gmail.com>
2017-03-23 21:23:51 +0200

    ajaxi parandus

--- webpage/application/views/haaleta.php
+++ webpage/application/views/haaleta.php
@@ -1,18 +1,16 @@
-
 <div class="container">
   
-  <form action="#">
+
   <br>
 	<label for="kandidaadi_id">Sisesta kandidaadi id: <input type="text" id="kandidaadi_id" name="kandidaadi_id" value=""></label><br>
 	<br>
 <input type="submit" value="Hääleta" onclick="loadXMLDoc()">
 
-</form>
-<br>
+
+<br><br>
 <p id="demo"></p>
 
 
 </div>
 </body>
 </html>
-
