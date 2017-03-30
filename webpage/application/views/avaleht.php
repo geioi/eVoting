60dfffda70d6b1c6513a39e951241e676d38357e
Maria <mariabelinskaa@gmail.com>
2017-03-30 16:02:30 +0300

    xml keelte kooskasutus

--- webpage/application/views/avaleht.php
+++ webpage/application/views/avaleht.php
@@ -4,7 +4,19 @@
   <?php echo $map['js']; ?>
   <?php echo $map['html']; ?>
   <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDfopMg31GEOWmiqbWs-lqa-Bu_aCf4XFE&callback=initMap" type="text/javascript"></script>
-</div>
+<br><br><?php
+$xml=simplexml_load_file("http://localhost/eVoting/webpage/xml/file.xml");
+
+foreach ($xml->children() as $erakond) {
+    echo 'Erakond: ' . $erakond['nimi']."\n<br />"; 
+
+    foreach ($erakond->children() as $arve) {
+        echo 'Arve: ' . $arve['pangaarve']."\n<br />\n<br />";
+    }
+}
+?>
+  
+  </div>
 </body>
 </html>
 
